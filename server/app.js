50071b7f3256dc16b1deaba7d49342cea8bb9658
Jonathan Kaufman <jondakauf@gmail.com>
2015-12-03 17:50:51 -0500

    enable CORS

--- server/app.js
+++ server/app.js
@@ -47,6 +47,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, '../public')));
+// enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use('/', routes);
 
 /** Start her up, boys */
